f3b8b3a0ca16f99e530005e3c360ec4f6f5682a2
zoltan-molnar <rdigital4455@gmail.com>
2025-04-21 01:07:33 +0200

    1:00

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col gap-y-4">
+      <div>
+        <Button variant={"elevated"}>
+          I am a button
+        </Button>
+      </div>
+      <div>
+        <Input placeholder='I am an input'/>
+
+      </div>
      
     </div>
   );
